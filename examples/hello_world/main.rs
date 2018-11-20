9f3dd1733bd38ad6bac8440073c886021340919b
Julian Pokrovsky <juliankul@gmail.com>
2018-11-21 01:46:42 +0300

    update hello_world example

--- examples/hello_world/main.rs
+++ examples/hello_world/main.rs
@@ -20,10 +20,10 @@ fn main() {
     // Extract XSD messages information
     let messages = wsdl.messages;
 
-    let mut scope = Scope::new();
+    let mut types_scope = Scope::new();
 
     messages.iter().for_each(|message| {
-        let structure = scope.new_struct(&message.name);
+        let structure = types_scope.new_struct(&message.name);
 
         match &message.documentation {
             Some(doc) => structure.doc(doc.text.as_str()),
@@ -40,15 +40,15 @@ fn main() {
         });
     });
 
-    let target_file = env::current_dir().unwrap().join("examples/hello_world/").join("codegen.rs");
-    print_codegen(&scope, Some(target_file)).expect("Error while printing code.");
+    let types_file = env::current_dir().unwrap().join("examples/hello_world/").join("types.rs");
+    print_codegen(&types_scope, Some(types_file)).expect("Error while printing code.");
 }
 
-fn print_codegen(scope: &Scope, file: Option<PathBuf>) -> Result<(), std::io::Error> {
+fn print_codegen(types_scope: &Scope, file: Option<PathBuf>) -> Result<(), std::io::Error> {
     match file {
-        None => println!("Scope: {:#?}", scope),
+        None => println!("Scope: {:#?}", types_scope),
         Some(f) => {
-            File::create(f)?.write(scope.to_string().as_bytes())?;
+            File::create(f)?.write(types_scope.to_string().as_bytes())?;
         }
     }
 
